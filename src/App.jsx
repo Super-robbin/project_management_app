627d9c470bf11131b0b32bb0717046fcbfb9362c
Roberto <roberto.quadraccia90@gmail.com>
2024-01-10 14:17:30 +0100

    Input component setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ProjectsSideBar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
 
 const App = () => {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <ProjectsSideBar />
+      <NewProject />
     </main>
   );
 };
