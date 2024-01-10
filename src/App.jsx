98ce1d3e27e5a0876f89a0bb172ffbf522c1054e
Roberto <roberto.quadraccia90@gmail.com>
2024-01-10 13:54:52 +0100

    Sidebar styled with Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import ProjectsSideBar from "./components/ProjectsSidebar";
 
 const App = () => {
   return (
-    <>
+    <main className="h-screen my-8">
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <ProjectsSideBar />
-    </>
+    </main>
   );
-}
+};
 
 export default App;
