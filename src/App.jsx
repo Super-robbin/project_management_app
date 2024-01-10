b9b712deb28e57190ba2a958706fb99c8806dfad
Roberto <roberto.quadraccia90@gmail.com>
2024-01-10 14:45:40 +0100

    No project and button components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ProjectsSideBar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 const App = () => {
   return (
     <main className="h-screen my-8 flex gap-8">
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 };
