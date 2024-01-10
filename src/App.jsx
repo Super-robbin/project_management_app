ea9d50d9418ef784b55cee2bd68dff7a85cd3044
Roberto <roberto.quadraccia90@gmail.com>
2024-01-10 13:45:48 +0100

    Sidebar setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectsSideBar from "./components/ProjectsSidebar";
+
+const App = () => {
+  return (
+    <>
+      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
+      <ProjectsSideBar />
+    </>
+  );
+}
+
+export default App;
