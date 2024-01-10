f94e9881922083928150411e15e5903bbf6496e3
Roberto <roberto.quadraccia90@gmail.com>
2024-01-10 14:59:37 +0100

    Conditional rendering No and New project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import ProjectsSideBar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 const App = () => {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  // undefined means we are doing nothing, null means we are adding a new project
+
+  const handleStartAddProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <ProjectsSideBar />
-      <NoProjectSelected />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 };
